e479f4d95e922ae8e4a891a088b6730edbb3d22c
Everton Vaszelewski <everton.vaszelewski@gmail.com>
2021-01-20 15:22:00 -0300

    Add comments to posts

--- src/main/java/com/vaszelewski/workshopmongo/domain/Post.java
+++ src/main/java/com/vaszelewski/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.vaszelewski.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.vaszelewski.workshopmongo.dto.AuthorDTO;
+import com.vaszelewski.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -20,6 +23,8 @@ public class Post implements Serializable{
 	private String body;
 	private AuthorDTO author;
 
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 	public Post() {
 	}
 
@@ -71,6 +76,14 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -96,7 +109,6 @@ public class Post implements Serializable{
 			return false;
 		return true;
 	}
-	
 	
 
 }
